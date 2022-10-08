ff849acedc2b34d8c21f8a10e9ff441ab9ccb8ca
Dijoy1866 <113097359+Dijoy1866@users.noreply.github.com>
2022-10-08 10:32:52 +0300

    9

---
+++ hw 9.4.py
@@ -0,0 +1,20 @@
+
+#Задание 4:
+#Запросить у пользователя число N
+#Запросить у пользователя N чисел и записать их в список A
+#Вывести список в обратной последовательности
+
+#OK
+
+N = int(input('Число пользователя N:' ))
+number = []
+
+for num in range(N):
+    x = int(input('Введите число: '))
+    number.append(x)
+    number_rev = list(reversed(number))
+
+print('A =' , number)
+print(number_rev)
+
+
